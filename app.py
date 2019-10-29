819daa35e87ee8d1200f686b382f1ab80cc4d194
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-10-29 15:14:08 -0700

    Build the bot with Slacker

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
+import bot
 import os
-import slack
 
-@slack.RTMClient.run_on(event='message')
-def say_hello(**payload):
-    data = payload['data']
-    web_client = payload['web_client']
-    rtm_client = payload['rtm_client']
-    if 'Hello' in data.get('text', []):
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
 
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Hi <@{user}>!",
-            thread_ts=thread_ts
-        )
+token = os.environ["SLACK_BOT_TOKEN"]
+bot = bot.Bot("transbot", token)
 
-slack_token = os.environ['SLACK_API_TOKEN']
-rtm_client = slack.RTMClient(token=slack_token)
-rtm_client.start()
--- bot.py
+++ bot.py
@@ -1,21 +1,68 @@
-import os
-import slack
+import websocket
+import json
+
+from slacker import Slacker
+
+
+class InvalidBot(Exception):
+    pass
+
+
+class ConnectionFailed(Exception):
+    pass
+
 
 class Bot:
-
     def __init__(self, name, token):
-        self.token = token
         self.name = name
+        self.client = Slacker(token)
+        self.connect()
+
+        self.user = self.get_user()
+        self.run()
+
+    def get_user(self):
+        for user in self.client.users.list().body["members"]:
+            if user["name"] == self.name:
+                return user
+
+        if not self.user:
+            raise InvalidBot(f"Bot with name {self.name} is not found")
+
+    def connect(self):
+        # Check for success
+        if self.client.api.test().successful:
+            print(
+                f"{self.name} Connected to {self.client.team.info().body['team']['name']}."
+            )
+        else:
+            raise ConnectionFailed("Connection failed please try again!")
+
+    def handle(self, event):
+        event = json.loads(event)
+
+        if event["type"] != "message":
+            return
+
+        if event["text"].startswith(f"<@{self.user['id']}>"):
+            print(event["text"].replace(f"<@{self.user['id']}>", ""))
+
+    def post_message(self, channel, message):
+        self.client.chat.post_message(
+            channel=channel, text=message,
+        )
+
+    def upload(self, file, title, comment, channel):
+        self.client.files.upload(
+            file_=file, channels=[channel], title=title, initial_comment=comment
+        )
 
     def run(self):
-        while True:            
-            rtm_client = slack.RTMClient(token=self.token)
-            rtm_client.start()
-
-    @slack.RTMClient.run_on(event='message')
-    def handle(self, event):
-        print(event)
-
-
-
+        rtm = self.client.rtm.start()
+        url = rtm.body["url"]
+        ws = websocket.WebSocket()
+        ws.connect(url)
+        while True:
+            message = ws.recv()
+            self.handle(message)
 

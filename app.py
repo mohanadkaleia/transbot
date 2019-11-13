63244e1bb33cdb8ce20596f043a1bd00a30e34e8
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-11-13 13:34:37 -0800

    Wile -> while

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ log = logger.get_logger(__name__)
 
 token = os.environ["SLACK_BOT_TOKEN"]
 sheet_url = os.environ["SHEET_URL"]
-While True: 
+
+while True:
     try:
         bot = bot.Bot("transbot", token, sheet_url)
     except Exception as e:

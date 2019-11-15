e553b74cb1f8a7e40528353cc5a2e736837dce25
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-11-15 12:37:24 -0800

    Create a sample config file instead of the environment variable -- that was pain in the back

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import bot
 import sheet
 import os
 import logger
+import config
 
 log = logger.get_logger(__name__)
 
-token = os.environ["SLACK_BOT_TOKEN"]
-sheet_url = os.environ["SHEET_URL"]
+token = config.prod['slack_token']
+sheet_url = config.prod["sheet"]
 
 while True:
     try:
         bot = bot.Bot("transbot", token, sheet_url)
     except Exception as e:
         log.exception(e)
-
---
+++ config_sample.py
@@ -0,0 +1,8 @@
+'''
+Put your configuration here and move the file to config.py
+'''
+
+prod = {
+    'slack_token': 'PUT YOUR SLACK TOKEN HERE',
+    'sheet': 'https://your_google_sheet_url'
+}
